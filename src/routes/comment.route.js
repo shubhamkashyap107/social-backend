0a2f765228e1a8c4d437047fa0109f24d471b96d
somya Thakur <somyathakur040403@gmail.com>
2026-06-17 13:10:16 +0530

    Make delete router

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -2,6 +2,7 @@ const express = require("express")
 const { isLoggedIn } = require("../middlewares/isLoggedIn")
 const { Post } = require("../models/post.model")
 const router = express.Router()
+const { Comment} = require("../models/comment.model")
 const mongoose = require("mongoose")
 
 
@@ -46,6 +47,49 @@ router.post("/create-comment", isLoggedIn, async(req, res) => {
             err: error.message
         })
     }
+})
+
+router.delete("/:id", isLoggedIn, async(req, res) => {
+    try {
+            const foundUser = req.user
+            const {id} = req.params
+
+            const comment = await Comment.findById(id)
+
+            if (!comment) {
+              throw new Error("Comment not found")
+            }
+
+            const post = await Post.findById(comment.postId)
+
+            if (!post) {
+            throw new Error("Post not found")
+            }
+
+
+
+            if (!(comment.authorId.toString() !== foundUser._id.toString()) && !(post.authorId.toString() !== foundUser._id.toString())) {
+              throw new Error("You are not authorized to delete this comment")
+            }
+            
+            await Comment.findByIdAndDelete(commentId)
+
+            post.comments.pull(commentId)
+            await post.save()
+
+            res.status(200).json({
+            success: true,
+            message: "Comment deleted successfully"
+            })
+
+
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            err: error.message
+        })
+    }
+
 })
 
 module.exports = {
